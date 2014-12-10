77fa902e9b50247746f3fc794223e04e6d9068b3
perfectsquircle <calvinfurano@gmail.com>
2014-12-10 11:50:29 -0700

    copy files to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,24 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		jshint: {
 			files: [ "**/*.js", "!node_modules/**", "!bower_components/**" ]
+		},
+		clean: {
+			dist: [ "dist" ]
+		},
+		copy: {
+			all: {
+				src: [ "**/*", "!Gruntfile", "!node_modules/**", "!bower_components/**" ],
+				dest: "./dist",
+				expand: true
+			}	
 		}
 	});
 	
 	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-copy");
+	grunt.loadNpmTasks("grunt-contrib-clean");
 	
-	grunt.registerTask("default", [ "jshint" ]);
+	grunt.registerTask("default", [ "jshint", "clean", "copy" ]);
 	grunt.registerTask("dev", [ "jshint" ]);
 };
 
