80098a83b2000c26ce5c2e7cb1fc2bef55eabcd8
perfectsquircle <calvinfurano@gmail.com>
2014-12-17 08:37:50 -0700

    formatting

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,45 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		jshint: {
-			files: [ "**/*.js", "!node_modules/**", "!bower_components/**" ]
-		},
-		clean: {
-			dist: [ "dist", "<%= pkg.name %>-<%= pkg.version %>.tgz" ]
-		},
-		copy: {
-			all: {
-				src: [ "**/*", "!Gruntfile", "!node_modules/**", "!bower_components/**" ],
-				dest: "./dist",
-				expand: true
-			}	
-		},
-		compress: {
-			dist: {
-				options: {
-					archive: "<%= pkg.name %>-<%= pkg.version %>.tgz",
-					mode: "tgz"
-				},
-				cwd: "dist",
-				src: "**",
-				dest: "."
-			}
-		}
-	});
-	
-	grunt.loadNpmTasks("grunt-contrib-jshint");
-	grunt.loadNpmTasks("grunt-contrib-copy");
-	grunt.loadNpmTasks("grunt-contrib-clean");
-	grunt.loadNpmTasks('grunt-contrib-compress');
-	
-	grunt.registerTask("default", [ "jshint" ]);
-	grunt.registerTask("dist", [ "default", "clean", "copy" ]);
-	grunt.registerTask("package", [ "dist", "compress" ]);
-};
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            files: ["**/*.js", "!node_modules/**", "!bower_components/**"]
+        },
+        clean: {
+            dist: ["dist", "<%= pkg.name %>-<%= pkg.version %>.tgz"]
+        },
+        copy: {
+            all: {
+                src: ["**/*", "!Gruntfile", "!node_modules/**", "!bower_components/**"],
+                dest: "./dist",
+                expand: true
+            }
+        },
+        compress: {
+            dist: {
+                options: {
+                    archive: "<%= pkg.name %>-<%= pkg.version %>.tgz",
+                    mode: "tgz"
+                },
+                cwd: "dist",
+                src: "**",
+                dest: "."
+            }
+        },
+        jsbeautifier: {
+            files: ["**/*.js", "!node_modules/**", "!bower_components/**"],
+            options: {}
+        }
+    });
 
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-compress");
+    grunt.loadNpmTasks("grunt-jsbeautifier");
+
+    grunt.registerTask("default", ["jshint"]);
+    grunt.registerTask("dist", ["default", "clean", "copy"]);
+    grunt.registerTask("package", ["dist", "compress"]);
+
+    grunt.registerTask("dev", ["jsbeautifier", "jshint"]);
+};
