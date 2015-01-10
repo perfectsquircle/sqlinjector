7dba4cb67a43d43bab084dbd6cd58591e024001b
perfectsquircle <calvinfurano@gmail.com>
2015-01-10 16:52:30 -0700

    fixing schema view

--- lib/db/get-schema-pg.sql
+++ lib/db/get-schema-pg.sql
@@ -4,7 +4,7 @@ select
   relkind
 from pg_class pc
   join pg_namespace pn on pc.relnamespace = pn.oid
-where nspname not in ('information_schema', 'pg_catalog')
+where not (pn.nspname like 'pg_%' OR pn.nspname='information_schema')
 --and relkind in ('r','v')
 order by schema, relkind, relation
 ;
