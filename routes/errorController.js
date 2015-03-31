79da8e5f8e167295d13e529a52f1e3cf100fb405
perfectsquircle <calvinfurano@gmail.com>
2015-03-30 20:48:15 -0600

    Don't use console transport in prod. Don't use file transport in dev.

--- routes/errorController.js
+++ routes/errorController.js
@@ -3,7 +3,9 @@ var logger = require("../lib/logger");
 exports.errorHandler = function(error, req, res, next) {
     logger.error(error);
     if (req.xhr) {
-        res.status(500).json({ message: error.toString() });
+        res.status(500).json({
+            message: error.toString()
+        });
     } else {
         next(error);
     }
