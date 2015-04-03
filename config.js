2e22f7c6489506d641cef1999f88517904a0dd83
perfectsquircle <calvinfurano@gmail.com>
2015-04-03 10:29:00 -0600

    tweaks

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.staticAssetMaxAge = 0;
 exports.dbConnectionString = "postgres://calvin:postgres@localhost/sqlinjector";
 exports.passwordHashRounds = 10;
-exports.consoleSessionTimeout = 600000;
+exports.consoleSessionTimeout = 3600000;
 exports.prettyHtml = true;
 
 if (process.env.NODE_ENV === "production") {
